b9ef51b3b8b1c785f7591a50520b37f319fdd974
neonrights <nicholasfarn@gmail.com>
2017-03-05 17:44:42 -0800

    FIX image detection parsing error

--- pdf2graphs.py
+++ pdf2graphs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import sys
 import subprocess
 import argparse
@@ -15,6 +16,7 @@ parser.add_argument('-l','--last',action='store',nargs=1,default=[1023],help='la
 parser.add_argument('-g','--graphs',action='store_true',default=False,help='only extract vector image graphs from pdf')
 parser.add_argument('-i','--images',action='store_true',default=False,help='only extract images from pdf')
 parser.add_argument('-o','--output',action='store',nargs=1,default='png',help='filetype for image output, supports png, jpeg, and eps')
+parser.add_argument('--folder',action='store_true',default=False,help='specify folder of pdfs instead of pdf name')
 parser.add_argument('pdf')
 
 args = parser.parse_args()
@@ -38,7 +40,7 @@ def write_json(label,metadata,g,ofile):
   ofile.write("}}\n\n")
 
 
-def extract_graphs(page,eps_objects):
+def extract_graphs(page,eps_objects,folder='.'):
   graph = graph_guess(eps_objects)
   graphs = get_connected_embedded_subgraphs(graph)
 
@@ -48,9 +50,9 @@ def extract_graphs(page,eps_objects):
     g.plot()
   
   if(len(graphs) > 0):
-    plt.savefig("page_%i.png" % ip)
+    plt.savefig("%s/page_%i.png" % (folder,ip))
     plt.clf()
-    ofile_name = "page_%i.json" % ip
+    ofile_name = "%s/page_%i.json" % (folder,ip)
     ofile = open(ofile_name,"w")
 
     metadata = [["comments", "Graph found on page %i of %s" % (ip,args.pdf)],\
@@ -64,7 +66,7 @@ def extract_graphs(page,eps_objects):
     ofile.close()
 
 
-def extract_images(page,lines,eps_objects,output='png'):
+def extract_images(page,lines,eps_objects,folder='.',output='png'):
   header1, header2 = get_headers(lines)
   footer = get_footer(lines)
 
@@ -77,79 +79,118 @@ def extract_images(page,lines,eps_objects,output='png'):
 
   # extract images
   for i,image in enumerate(images):
-    image_name = "%s-%d-%d" % (args.pdf[:-4],page,i+1)
-    ofile = open("%s.eps" % image_name,'w+')
-    for line in header1:
-      ofile.write(line)
+    if image.encoded is not None: 
+      image_name = "%d-%d" % (page,i+1)
+      ofile = open("%s/%s.eps" % (folder,image_name),'w+')
+      for line in header1:
+        ofile.write(line)
 
-    ofile.write("%%%%BoundingBox: 0 0 %s %s\n" % (image.setup.values[1],image.setup.values[2]))
-    for line in header2:
-      ofile.write(line)
+      ofile.write("%%%%BoundingBox: 0 0 %s %s\n" % (image.setup.values[1],image.setup.values[2]))
+      for line in header2:
+        ofile.write(line)
 
-    # write image
-    ofile.write("%s setcolorspace\n" % image.setup.values[11])
-    ofile.write("8 dict dup begin\n")
-    ofile.write("  /ImageType %s def\n" % image.setup.values[0])
-    ofile.write("  /Width %s def\n" % image.setup.values[1])
-    ofile.write("  /Height %s def\n" % image.setup.values[2])
-    if image.setup.values[3]:
-      ofile.write("  /Interpolate %s def\n" % str(image.setup.values[3]).lower())
+      # write image
+      if image.setup.values[11] is not None:
+        ofile.write("%s setcolorspace\n" % image.setup.values[11])
+      ofile.write("8 dict dup begin\n")
+      ofile.write("  /ImageType %s def\n" % image.setup.values[0])
+      ofile.write("  /Width %s def\n" % image.setup.values[1])
+      ofile.write("  /Height %s def\n" % image.setup.values[2])
+      if image.setup.values[3] is not None:
+        ofile.write("  /Interpolate %s def\n" % str(image.setup.values[3]).lower())
 
-    ofile.write("  /BitsPerComponent %s def\n" % image.setup.values[4])
-    ofile.write("  /Decode [ %s ] def\n" % ' '.join(image.setup.values[5]))
-    ofile.write("  /DataSource %s /ASCII85Decode %s %s %s def\n" % tuple(image.setup.values[6:10]))
-    ofile.write("  /ImageMatrix [ %s ] def\n" % ' '.join(image.setup.values[10]))
-    ofile.write("end\nimage\n")
-    ofile.write('\n '.join(image.encoded))
-    if image.encoded[-1][-2:] == '~>':
-      ofile.write('\nQ\n')
-    else:
-      ofile.write('\n')
+      ofile.write("  /BitsPerComponent %s def\n" % image.setup.values[4])
+      ofile.write("  /Decode [ %s ] def\n" % ' '.join(image.setup.values[5]))
+      ofile.write("  /DataSource %s /ASCII85Decode %s %s %s def\n" % tuple(image.setup.values[6:10]))
+      ofile.write("  /ImageMatrix [ %s ] def\n" % ' '.join(image.setup.values[10]))
+      ofile.write("end\n%s\n" % image.setup.values[12])
+      ofile.write('\n '.join(image.encoded))
+      if image.encoded[-1][-2:] == '~>':
+        ofile.write('\nQ\n')
+      else:
+        ofile.write('\n')
 
-    for line in footer:
-      ofile.write(line)
+      for line in footer:
+        ofile.write(line)
 
-    ofile.close()
+      ofile.close()
 
-    if output != 'eps':
-      batcmd2 = "convert %s.eps %s.%s" % (image_name,image_name,output)
-      result2 = subprocess.check_output(batcmd2, shell=True)
+      if output != 'eps':
+        batcmd2 = "convert %s/%s.eps %s/%s.%s" % (folder,image_name,folder,image_name,output)
+        result2 = subprocess.check_output(batcmd2, shell=True)
   
 
 if __name__ == '__main__':
-  # get number of pages for pdf
-  batcmd="pdfinfo %s | grep -i Pages" % args.pdf
-  result = subprocess.check_output(batcmd, shell=True)
-  npages = int(str(result).split()[1].replace("\\n'",""))
-  print("PDF has", npages, "pages")
+  if args.folder:
+    for filename in os.listdir(args.pdf):
+      if filename.endswith('.pdf'):
+        # get number of pages for pdf
+        batcmd="pdfinfo %s/%s | grep -i Pages" % (args.pdf,filename)
+        result = subprocess.check_output(batcmd, shell=True)
+        npages = int(str(result).split()[1].replace("\\n'",""))
+        print(filename,"has", npages,"pages")
 
-  first_page = max(1,min(int(args.first[0]),npages))
-  last_page = min(npages,max(int(args.last[0]),1)) + 1
-  
-  # for each pdf page, remove text and convert to jpeg
-  images = []
-  for ip in range(first_page,last_page):
-    print("Converting page %i" % ip)
-    batcmd1 = "pdftocairo -f %i -l %i -eps %s page.eps" % (ip,ip,args.pdf)
-    result1 = subprocess.check_output(batcmd1, shell=True)
+        if not os.path.exists(filename[:-4]):
+          os.makedirs(filename[:-4])
 
-    ifile = open("page.eps")
-    lines = ifile.readlines()
-    ifile.close()
+        # for each pdf page, remove text and convert to jpeg
+        images = []
+        for ip in range(1,npages+1):
+          print("Converting page %i" % ip)
+          batcmd1 = "pdftocairo -f %i -l %i -eps %s/%s page.eps" % (ip,ip,args.pdf,filename)
+          result1 = subprocess.check_output(batcmd1, shell=True)
 
-    temp = remove_text(lines)
-    temp = remove_resources(temp)
-    temp = remove_page_setup(temp)
-    temp = remove_remainder(temp)
+          ifile = open("page.eps")
+          lines = ifile.readlines()
+          ifile.close()
 
-    content = ' '.join(lines)
-    eps_objects = get_eps_objects(content)
+          temp = remove_text(lines)
+          temp = remove_resources(temp)
+          temp = remove_page_setup(temp)
+          temp = remove_remainder(temp)
 
-    if args.graphs or not args.images:
-      extract_graphs(ip,eps_objects)
+          content = ' '.join(lines)
+          eps_objects = get_eps_objects(content)
 
-    if not args.graphs or args.images:
-      extract_images(ip,lines,eps_objects,output=args.output)
+          if args.graphs or not args.images:
+            extract_graphs(ip,eps_objects,folder=filename[:-4])
+
+          if not args.graphs or args.images:
+            extract_images(ip,lines,eps_objects,folder=filename[:-4],output=args.output)
+  else: 
+    # get number of pages for pdf
+    batcmd="pdfinfo %s | grep -i Pages" % args.pdf
+    result = subprocess.check_output(batcmd, shell=True)
+    npages = int(str(result).split()[1].replace("\\n'",""))
+    print("PDF has", npages, "pages")
+
+    first_page = max(1,min(int(args.first[0]),npages))
+    last_page = min(npages,max(int(args.last[0]),1)) + 1
+
+    # for each pdf page, remove text and convert to jpeg
+    images = []
+    for ip in range(first_page,last_page):
+      print("Converting page %i" % ip)
+      batcmd1 = "pdftocairo -f %i -l %i -eps %s page.eps" % (ip,ip,args.pdf)
+      result1 = subprocess.check_output(batcmd1, shell=True)
+
+      ifile = open("page.eps")
+      lines = ifile.readlines()
+      ifile.close()
+
+      temp = remove_text(lines)
+      temp = remove_resources(temp)
+      temp = remove_page_setup(temp)
+      temp = remove_remainder(temp)
+
+      content = ' '.join(lines)
+      eps_objects = get_eps_objects(content)
+
+      if args.graphs or not args.images:
+        extract_graphs(ip,eps_objects)
+
+      if not args.graphs or args.images:
+        extract_images(ip,lines,eps_objects,output=args.output)
 
   result3 = subprocess.check_output("rm -f *.eps", shell=True)
 
