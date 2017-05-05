b39d56172a89d20ac8daff9c3bfcf26279087488
William Casper <riley_c2003@yahoo.com>
2017-05-04 17:00:32 -0700

    fixed several issues with graph interpreter

--- epsinterpreter.py
+++ epsinterpreter.py
@@ -77,36 +77,120 @@ def get_eps_objects(content):
 
       objects.append(image)
 
-    elif("c" in block or "l" in block):
+    elif("c" in block or "l" in block or "re" in block):
       path = EPSVectorPath()
       blen = len(block)
+      # affine transformation
+      a = 1
+      b = 0
+      c = 0
+      d = 1
+      tx = 0
+      ty = 0
       for i in range(blen):
         val = block[i]
+        if(val == "cm"):
+          # get the transformation matrix
+          a = float(block[i-6])
+          b = float(block[i-5])
+          c = float(block[i-4])
+          d = float(block[i-3])
+          tx = float(block[i-2])
+          ty = float(block[i-1])
+
         if(val == "c"):
-          path.x.append(float(block[i-6]))
-          path.x.append(float(block[i-4]))
-          path.x.append(float(block[i-2]))
-          path.y.append(float(block[i-5]))
-          path.y.append(float(block[i-3]))
-          path.y.append(float(block[i-1]))
+          x0 = float(block[i-6])
+          y0 = float(block[i-5])
+          x = a*x0 + c*y0 + tx
+          y = b*x0 + d*y0 + ty
+          path.x.append(x)
+          path.y.append(y)
+
+          x0 = float(block[i-4])
+          y0 = float(block[i-3])
+          x = a*x0 + c*y0 + tx
+          y = b*x0 + d*y0 + ty
+          path.x.append(x)
+          path.y.append(y)
+
+          x0 = float(block[i-2])
+          y0 = float(block[i-1])
+          x = a*x0 + c*y0 + tx
+          y = b*x0 + d*y0 + ty
+          path.x.append(x)
+          path.y.append(y)
+
           path.t.append("c")
+
         elif(val == "l"):
-          path.x.append(float(block[i-2]))
-          path.y.append(float(block[i-1]))
+          x0 = float(block[i-2])
+          y0 = float(block[i-1])
+          x = a*x0 + c*y0 + tx
+          y = b*x0 + d*y0 + ty
+          path.x.append(x)
+          path.y.append(y)
+
           path.t.append("l")
+
         elif(val == "m"):
-          path.x.append(float(block[i-2]))
-          path.y.append(float(block[i-1]))
+#          # everytime we move, we "lift the pen"
+#          # starting a new path
+          if (len(path.x) > 0):
+            path.n = len(path.x)
+            objects.append(path.copy())
+            path = EPSVectorPath()
+            
+          x0 = float(block[i-2])
+          y0 = float(block[i-1])
+          x = a*x0 + c*y0 + tx
+          y = b*x0 + d*y0 + ty
+          path.x.append(x)
+          path.y.append(y)
+          
           path.t.append("m")
+
         elif(val == "g"):
-          path.color = float(block[i-1])
+          path.color = (float(block[i-1]))
         elif(val == "f" or val == "f*"):
           path.filled = True
-        elif(val == "f*" or val == "h"):
+          path.completed = True
+          path.x.append(path.x[0])
+          path.y.append(path.y[0])
+          path.t.append("l")
+        elif(val == "h"):
+          path.completed = True
+          path.x.append(path.x[0])
+          path.y.append(path.y[0])
+          path.t.append("l")
+        elif(val == "re"):
+          x0 = float(block[i-4])
+          y0 = float(block[i-3])
+          dx0 = float(block[i-2])
+          dy0 = float(block[i-1])
+          x = a*x0 + c*y0 + tx
+          y = b*x0 + d*y0 + ty
+          dx = a*dx0 + c*dy0
+          dy = b*dx0 + d*dy0
+          path.x.append(x)
+          path.y.append(y)
+          path.t.append("l")
+          path.x.append(x+dx)
+          path.y.append(y)
+          path.t.append("l")
+          path.x.append(x+dx)
+          path.y.append(y+dy)
+          path.t.append("l")
+          path.x.append(x)
+          path.y.append(y+dy)
+          path.t.append("l")
+          path.x.append(x)
+          path.y.append(y)
+          path.t.append("l")
           path.completed = True
 
       path.n = len(path.x)
-      objects.append(path)
+      if(path.n > 1):
+        objects.append(path)
 
   return objects
 
--- graph_guess.py
+++ graph_guess.py
@@ -16,93 +16,112 @@ def graph_guess(objects):
   nobjects = len(objects)
   guess = [""]*nobjects
   g = EmbeddedGraph()
+  xs = []
+  ys = []
   sizes = []  # node symbol sizes
+  indices=[]  # symbol indices
   # first get all the nodes
   for iobj in range(nobjects):
     obj = objects[iobj]
     if(isinstance(obj,EPSVectorPath)):
       # if the path is completed and curved, it probably is a node
-      if(obj.completed and "c" in obj.t and not "l" in obj.t):
+      # We will guess that an object is a node if any of the following
+      # sets of conditions hold for the  object
+      # 1) the object is curved, filled, and small
+      # 2) the object is completed, curved, and small
+      node_conditions_fulfilled = False
+      if(obj.filled and "c" in obj.t):
+        node_conditions_fulfilled = True
+      if(obj.completed and "c" in obj.t):
+        node_conditions_fulfilled = True
+       
+      if(node_conditions_fulfilled):
         x = np.mean(obj.x)
         y = np.mean(obj.y)
         u = np.array(obj.x)-x
         v = np.array(obj.y)-y
         # estimate node symbol size
         sizes.append(np.mean(np.sqrt(u*u+v*v)))
-        g.add_node(x,y)
+        indices.append(iobj)
+        xs.append(x)
+        ys.append(y)
         guess[iobj] = "node"
 
+  # we expect nodes to be of uniform size
+  # here we throw away any nodes whose size is more than
+  # twenty percent of the mean
+  nnodes = len(sizes)
+  if (nnodes > 1):
+    meansize = np.mean(sizes)
+    for i in range(nnodes-1,-1,-1):
+      if(sizes[i] < meansize*1.2): 
+        g.add_node(xs[i],ys[i])
+      else:
+        guess[indices[i]] = ""
+        del sizes[i]
+        del indices[i]
+        del xs[i]
+        del ys[i]
+      
   # next get all the edges
   for iobj in range(nobjects):
     obj = objects[iobj]
+    connected_nodes = []
+    connected_posit = []
     if(isinstance(obj,EPSVectorPath)):
       # if the path is not completed, it probably is an edge
       # we should therefore connect any nodes occuring as
       # coordinates in order in the sequence of the path
       if(guess[iobj] != "node" and obj.color < 0.01):
-
-#        # old algorithm
-#        v = [-1]*obj.n
-#        for i in range(obj.n):
-#          x = obj.x[i]
-#          y = obj.y[i]
-#          idx,ds = g.nearest_node(x,y)
-#          if(ds < 10):
-##            v[i] = idx
-#            if(i >= 0 and v[i-1] >= 0):
-#              g.add_edge(v[i],v[i-1])
-
-        # new algorithm
-        # approximate distance of each node from the vector path
-        # by using a piecewise linear approximation
-        # keep track of this ``closest point on curve``
-        dists = [0.0]*g.n
-        segment = [-1]*g.n
-        wgt = [0.0]*g.n
-        for i in range(g.n):
-          x=g.x[i]
-          y=g.y[i]
-          dists[i] = 1e10
-          for j in range(obj.n-1):
-            u1=np.array([obj.x[j]-x,obj.y[j]-y])
-            u2=np.array([obj.x[j+1]-x,obj.y[j+1]-y])
+        for j in range(obj.n-1):
+          x1 = obj.x[j]
+          y1 = obj.y[j]
+          x2 = obj.x[j+1]
+          y2 = obj.y[j+1]
+          for i in range(g.n):
+            x=g.x[i]
+            y=g.y[i]
+            u1=np.array([x1-x,y1-y])
+            u2=np.array([x2-x,y2-y])
             v=(u2-u1)
             nn=np.linalg.norm(v)
-            if(nn < 1e-6):
-              nn = -1.0
-            v=v/nn
+            dd = 1e10
+            ww = 0.01
             d1=np.linalg.norm(u1)
             d2=np.linalg.norm(u2)
-            d3=np.linalg.norm(u1-np.dot(v,u1)*v)
-            if((d3 < d1 and d3 < d2) or nn < 0):
+            if(nn < 1e-2):
               if(d1 < d2):
                 dd = d1
-                ww = 0.0
+                ww = 0.01
               else:
                 dd = d2
-                ww = 1.0
+                ww = 0.99
             else:
-              dd = d3
-              ww = np.abs(np.dot(v,u1))/nn
-            if(dd < dists[i]):
-              dists[i] = max(dd-sizes[i],0)
-              segment[i] = j
-              wgt[i] = ww
+              v=v/nn
+              comp = np.dot(v,u1)
+              d3=np.linalg.norm(u1-comp*v)
+              ww = -comp/nn
+              if(ww < 0.01 or ww > 0.99):
+                if(d1 < d2):
+                  dd = d1
+                  ww = 0.01
+                else:
+                  dd = d2
+                  ww = 0.99
+              else:
+                dd = d3
+            if(dd-sizes[i]*1.2 < TOUCH_TOLERANCE):
+              connected_nodes.append(i)
+              connected_posit.append(ww + j)
 
         # connect approximate nodes in order of appearance
-        near_nodes = []
-        rank = []
-        for i in range(g.n):
-          if dists[i] < TOUCH_TOLERANCE:
-            near_nodes.append(i)
-            rank.append(segment[i]+wgt[i])
-        nn_num = len(near_nodes)
+        nn_num = len(connected_nodes)
         if(nn_num>1):
-          rank = np.array(rank)
-          near_nodes = np.array(near_nodes)
-          near_nodes = near_nodes[rank.argsort()]
-        for i in range(nn_num-1):
-          g.add_edge(near_nodes[i],near_nodes[i+1])
+          connected_posit = np.array(connected_posit)
+          connected_nodes = np.array(connected_nodes)
+          connected_nodes = connected_nodes[connected_posit.argsort()]
+          for i in range(nn_num-1):
+            g.add_edge(connected_nodes[i],connected_nodes[i+1])
 
         guess[iobj] = "edge"
 
